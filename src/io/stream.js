bc28014de3bdefeaaf5539b64038768d8ffe3c7f
Georgi Mirchev <gmirchev90@gmail.com>
2015-05-28 07:24:18 +0300

    Share the translated speech to text through the stream server with all participants in the current session

--- src/io/stream.js
+++ src/io/stream.js
@@ -7,13 +7,58 @@ export function setup(server) {
         labels: ['stream']
     })
 
-    var io2 = require('socket.io')(server.select('stream').listener)
-    io2.on('connection', function (socket) {
+    var users = {};
+    var numUsers = 0;
 
-        socket.emit('Oh hii2!');
+    var io = require('socket.io')(server.select('stream').listener)
+    io.on('connection', function (socket) {
+        var addedUser = false;
 
-        socket.on('burp', function () {
-            socket.emit('Excuse you2!');
+        socket.on('stream', function (data, room) {
+            io.to(room).emit('stream', {
+                stream: data
+            })
+        })
+
+        socket.on('stream status', function(data, room) {
+            console.log(data, room)
+            io.to(room).emit('stream status', {
+                is_running: data
+            })
+        })
+
+        socket.on('add user', function (user, room) {
+            // we store the username in the socket session for this client
+            socket.join(room)
+            socket.user = user;
+            socket.room = room;
+            // add the client's username to the global list
+            users[user] = user;
+            ++numUsers;
+            addedUser = true;
+            socket.emit('login', {
+                numUsers: numUsers
+            });
+
+            socket.broadcast.emit('user joined', {
+                user: socket.user,
+                numUsers: numUsers
+            });
+        });
+
+        socket.on('disconnect', function () {
+            // remove the username from global usernames list
+            if (addedUser) {
+                delete users[socket.user];
+                --numUsers;
+
+                //difference between broadcast and without is that the event
+                //will not be broadcasted to the current socket if you use it
+                socket.broadcast.emit('user left', {
+                    user: socket.user,
+                    numUsers: numUsers
+                });
+            }
         });
     });
 }
